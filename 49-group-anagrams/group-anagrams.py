6dec8983fbe949b94f51fb4b29fbf84307485910
Giri Babu 242 <giribabu2715@gmail.com>
2026-09-02 11:44:06 +0530

    Time: 11 ms (82.73%) | Memory: 21.9 MB (79.32%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        d = {}
+
+        for s in strs:
+            key = ''.join(sorted(s))
+
+            if key not in d:
+                d[key] = []
+
+            d[key].append(s)
+
+        return list(d.values())
+        
